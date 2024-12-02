fc649628441c72d2878fde440d179cd93f48ff48
PRIY MAVANI <priy.mavani.cg@gmail.com>
2024-12-02 16:45:57 +0530

    adding login page to save userdata -storing input value with localstorage set.item -value retrieving data by button load userdata -submit button for saving userdata

---
+++ index.js
@@ -0,0 +1,55 @@
+let store = document.getElementById('store');
+
+store.addEventListener('mouseenter', ()=>{
+    store.style.color="orange";
+});
+store.addEventListener('mouseleave', ()=>{
+    store.style.color="white";
+});
+
+let launch = document.getElementById('new_lanch');
+
+launch.addEventListener('mouseenter', ()=>{
+    launch.style.color="orange";
+});
+launch.addEventListener('mouseleave', ()=>{
+    launch.style.color="white";
+});
+
+let phone = document.getElementById('realme_phone');
+
+phone.addEventListener('mouseenter', ()=>{
+    phone.style.color="orange";
+});
+phone.addEventListener('mouseleave', ()=>{
+    phone.style.color="white";
+});
+
+let narzo = document.getElementById('NARZO_Phone');
+
+narzo.addEventListener('mouseenter', ()=>{
+    narzo.style.color="orange";
+});
+narzo.addEventListener('mouseleave', ()=>{
+    narzo.style.color="white";
+});
+
+let audio = document.getElementById('Audio');
+
+audio.addEventListener('mouseenter', ()=>{
+    audio.style.color="orange";
+});
+audio.addEventListener('mouseleave', ()=>{
+    audio.style.color="white";
+});
+
+let accessories = document.getElementById('Accessories');
+
+accessories.addEventListener('mouseenter', ()=>{
+    accessories.style.color="orange";
+});
+accessories.addEventListener('mouseleave', ()=>{
+    accessories.style.color="white";
+});
+
+
